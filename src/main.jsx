8c96e2fabf8387142aa81cce2063010be1451f53
Cristi2692 <c.guerreroheras@gmail.com>
2023-12-12 22:19:24 +0100

    Primer Navbar completo de la aplicación.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import App from "./App.jsx";
+import "./index.css";
+import { theme } from "./theme.js";
+import { typeScale } from "./themeFonts.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const customTheme = createTheme({
+  ...theme,
+  typography: typeScale.typography,
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={customTheme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
---
+++ src/theme.js
@@ -0,0 +1,27 @@
+import { createTheme } from '@mui/material';
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#111111',
+      light: '#5A5A59',
+      dark: '#002D33',
+      contrastText: '#FFFFFF'
+    },
+    secondary: {
+      main: '#e1dfdf',
+      light: '#f0f0f0',
+      dark: '#898989',
+      contrastText: '#000000',
+     },
+     error: {
+      main: '#F05A39',
+    },
+    warning: {
+      main: '#FFCF60',
+    },
+    success: {
+      main: '#00B098',
+    },
+  },
+});
