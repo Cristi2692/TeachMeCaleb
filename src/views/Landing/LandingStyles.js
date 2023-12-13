8f48b24274dc4ae9aa631b0810176aec752ae55a
Cristi2692 <c.guerreroheras@gmail.com>
2023-12-13 21:55:15 +0100

    Mejora responsive

--- src/views/Landing/LandingStyles.js
+++ src/views/Landing/LandingStyles.js
@@ -3,8 +3,13 @@ import backgroundImage from '../../assets/flanding.png';
 
 export const StyledDiv = styled.div`
   background-image: url(${backgroundImage});
-  background-size: 70%;
+  background-size: 75%;
   background-repeat: no-repeat;
   background-position: center 15%;
   height: 100vh;
+
+  @media (max-width: 930px) {
+    background-size: 100%;
+    background-position: center 5%;
+  }
 `;
